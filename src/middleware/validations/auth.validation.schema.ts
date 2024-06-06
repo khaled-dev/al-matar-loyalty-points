4ac24d62bb4fca2ff0d1ce7ebdfe101b613dc732
Khaled AbdElRahim <khaled710017@gmail.com>
2024-06-06 06:53:39 +0300

    Add register email validation

--- src/middleware/validations/auth.validation.schema.ts
+++ src/middleware/validations/auth.validation.schema.ts
@@ -1,10 +1,18 @@
 import Joi from "joi";
-import {IUser} from "../../models/user.model";
+import User, {IUser, UserModel} from "../../models/user.model";
 
 export const registerValidationSchema =  Joi.object<IUser>({
-        name: Joi.string().required(),
-        email: Joi.string().required(),
-        password: Joi.string().required(),
+        name: Joi.string().required().max(225),
+        email: Joi.string().required().email()
+            .external(async (value, helpers) => {
+                let user : UserModel = await User.findOne({ email: value }).exec()
+
+                if (user) {
+                    return helpers.message( {external: '{#label} already exists' });
+                }
+                return true;
+            }),
+        password: Joi.string().required().min(5).max(225),
     });
 
 export const loginValidationSchema =  Joi.object<IUser>(
