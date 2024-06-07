54a50eec77b9a5d5be282d54bc529d2740fc5c23
Khaled AbdElRahim <khaled710017@gmail.com>
2024-06-07 20:31:24 +0300

    unit test transaction cron job

--- src/jobs/transaction.job.ts
+++ src/jobs/transaction.job.ts
@@ -10,38 +10,7 @@ const rejectTransactions = async () => {
         Logging.info('..CronJob Running..')
         Logging.info('...................')
 
-        const tenMinutesAgo : Date  = new Date(Date.now() - ( Number(process.env.TRANSACTION_EXPIRE_TIME) * 60 * 1000))
-        
-        // get all [old&pending] transactions
-        const transactions : TransactionModel[]   = await Transaction.find({
-            status: TransactionStatus.PENDING,
-            createdAt: {$lt: tenMinutesAgo}
-        })
-
-        // if nothing found
-        if (transactions.length === 0) {
-            Logging.info('CronJob: no transactions found...')
-            return;
-        }
-
-        // reject old transactions
-        const transactionIds : string[] = transactions.map((transaction: TransactionModel) => {return transaction.id })
-        await Transaction.updateMany({
-            _id: {$in: transactionIds},
-        }, {
-            status: TransactionStatus.REJECTED,
-        })
-        Logging.info('CronJob: transactions updated successfully ids: ' + transactionIds)
-
-        // resend points to its original sender
-        transactions.map( async (transaction : TransactionModel) => {
-            let user : UserModel = await User.findOne({email: transaction.senderEmail})
-            user.points += transaction.points
-            await user.save()
-
-            Logging.info('CronJob: user id: ' + user.id + ' updated successfully.')
-        })
-
+        await doRejectTransactions()
 
         Logging.info('...................')
         Logging.info('...CronJob: Done...')
@@ -50,4 +19,40 @@ const rejectTransactions = async () => {
     })
 }
 
-export default {rejectTransactions}
+const doRejectTransactions = async () : Promise<void> => {
+    const expireTime : number = process.env.TRANSACTION_EXPIRE_TIME ? Number(process.env.TRANSACTION_EXPIRE_TIME) : 10
+
+    const tenMinutesAgo : Date  = new Date(Date.now() - ( expireTime * 60 * 1000))
+
+    // get all [old&pending] transactions
+    const transactions : TransactionModel[]   = await Transaction.find({
+        status: TransactionStatus.PENDING,
+        createdAt: {$lt: tenMinutesAgo}
+    })
+
+    // if nothing found
+    if (transactions.length === 0) {
+        Logging.info('CronJob: no transactions found...')
+        return;
+    }
+
+    // reject old transactions
+    const transactionIds : string[] = transactions.map((transaction: TransactionModel) => {return transaction.id })
+    await Transaction.updateMany({
+        _id: {$in: transactionIds},
+    }, {
+        status: TransactionStatus.REJECTED,
+    })
+    Logging.info('CronJob: transactions updated successfully ids: ' + transactionIds)
+
+    // resend points to its original sender
+    transactions.map( async (transaction : TransactionModel) => {
+        let user : UserModel = await User.findOne({email: transaction.senderEmail})
+        user.points += transaction.points
+        await user.save()
+
+        Logging.info('CronJob: user id: ' + user.id + ' updated successfully.')
+    })
+}
+
+export default {rejectTransactions, doRejectTransactions}
