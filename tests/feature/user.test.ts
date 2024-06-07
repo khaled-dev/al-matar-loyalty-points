5ca13b1e2e675ad862f4bbf7351e932169ce239b
Khaled AbdElRahim <khaled710017@gmail.com>
2024-06-07 10:06:10 +0300

    Add user points test

---
+++ tests/feature/user.test.ts
@@ -0,0 +1,41 @@
+import request from 'supertest';
+import app from '../../src/server';
+import User, {UserModel} from "../../src/models/user.model";
+import Transaction, {TransactionModel} from "../../src/models/transaction.model";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import mongoose from "mongoose";
+
+describe('user', () => {
+
+    describe('points', () => {
+        it('should get his points number', async () => {
+            const user : UserModel = await User.create({
+                name: 'transaction Sender',
+                email: 'transactionSender@email.com',
+                password: await bcrypt.hash('password', 10),
+            })
+            let userToken = jwt.sign({ _id: user._id, email: user.email }, process.env.JWT_SECRET!, { expiresIn: process.env.JWT_TOKEN_EXPIRE });
+
+            const res = await request(app)
+                .get('/user/points')
+                .set('Authorization', 'Bearer ' + userToken)
+                .send()
+
+            expect(res.statusCode).toEqual(200)
+            expect(res.body.message).toEqual('User Points')
+            expect(res.body.data.Points).toEqual(500)
+        })
+    })
+
+    afterEach(async () => {
+        await User.deleteMany()
+    })
+
+    afterAll( () => {
+        mongoose.disconnect()
+    })
+
+})
+
+
